51f3069929daf2b64d8157c8f82f3ba57c2eb76b
tukeks <59303955+tukeks@users.noreply.github.com>
2020-02-01 01:35:15 +0900

    test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App"><h1>Hello World!</h1></div>
+    <div><h1>Hello, World!!!</h1></div>
   );
+
 }
 
+
+// class App extends React.Component{
+//   render(){   
+//     return (
+//      <div className="App"><h1>Hello, World!!</h1></div>
+//     );
+//   }
+// }
 export default App;
