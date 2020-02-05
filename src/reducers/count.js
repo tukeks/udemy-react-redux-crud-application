67f50e2115878bfea07e1c32ac877e7248e13f88
tukeks <59303955+tukeks@users.noreply.github.com>
2020-02-05 22:27:43 +0900

    implement reducer

---
+++ src/reducers/count.js
@@ -0,0 +1,17 @@
+import { INCREMENT, DECREMENT } from '../actions'
+
+const initialState = { value: 0}
+export default (state = initialState, action) => {
+
+    // こういった曖昧型は利用したくない。
+    switch(action.type) {
+        case INCREMENT:
+            return { value: state.value + 1 }
+
+        case DECREMENT:
+            return { value: state.value -1 }
+
+        default:
+            return;
+    }
+}
